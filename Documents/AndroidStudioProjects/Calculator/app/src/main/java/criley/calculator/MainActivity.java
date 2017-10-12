dc59e4c422fb6e7ad698409321edf9248040deaf
Colin <32081669+crileyy@users.noreply.github.com>
2017-10-12 13:12:42 -0400

    Add files via upload

--- Documents/AndroidStudioProjects/Calculator/app/src/main/java/criley/calculator/MainActivity.java
+++ Documents/AndroidStudioProjects/Calculator/app/src/main/java/criley/calculator/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+  private int output = 0;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -15,63 +17,116 @@ public class MainActivity extends AppCompatActivity {
   }
 
   public void clear(View view) {
-    TextView output = (TextView) findViewById(R.id.answer);
-
-    output.setText("");
+    TextView display = (TextView) findViewById(R.id.answer);
+    this.output = 0;
+    display.setText("");
   }
 
   public void negative(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
 
+    display.setText("");
   }
 
   public void percent(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void divide(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numSeven(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numEight(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numNine(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void multiply(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numFour(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numFive(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numSix(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void subtract(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numOne(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numTwo(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numThree(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void addition(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void numZero(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void addDecimal(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 
   public void equals(View view) {
+    TextView display = (TextView) findViewById(R.id.answer);
+
+    display.setText("");
   }
 }
